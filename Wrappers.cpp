5b43f582b31fca2115f03b0fb4e6f6710519b8e2
brain-tree <cloneno2@gmail.com>
2024-04-04 21:40:20 +0000

    Moved all wrapper class implementations outside of its class

--- Wrappers.cpp
+++ Wrappers.cpp
@@ -7,3 +7,45 @@ WrapperSynthesizer::~WrapperSynthesizer()
 {
     delete pointerToSynthesizer;
 }
+
+WrapperKeyboard::WrapperKeyboard(Synthesizer::Keyboard* keysptr) : pointerToKeyboard(keysptr) {}
+
+WrapperKeyboard::~WrapperKeyboard()
+{
+    delete pointerToKeyboard;
+}
+
+WrapperRestaurant::WrapperRestaurant(Restaurant* restptr) : pointerToRestaurant(restptr) {}
+
+WrapperRestaurant::~WrapperRestaurant()
+{
+    delete pointerToRestaurant;
+}
+
+WrapperKitchen::WrapperKitchen(Restaurant::Kitchen* kitptr) : pointerToKitchen(kitptr) {}
+
+WrapperKitchen::~WrapperKitchen()
+{
+    delete pointerToKitchen;
+}
+
+WrapperBank::WrapperBank(Bank* bankptr) : pointerToBank(bankptr) {}
+
+WrapperBank::~WrapperBank()
+{
+    delete pointerToBank;
+}
+
+WrapperEffectsRack::WrapperEffectsRack(EffectsRack* fxptr) : pointerToEffectsRack(fxptr) {}
+
+WrapperEffectsRack::~WrapperEffectsRack()
+{
+    delete pointerToEffectsRack;
+}
+
+WrapperDiningRoom::WrapperDiningRoom(DiningRoom* drptr) : pointerToDiningRoom(drptr) {}
+
+WrapperDiningRoom::~WrapperDiningRoom()
+{
+    delete pointerToDiningRoom;
+}
--- Wrappers.h
+++ Wrappers.h
@@ -15,66 +15,48 @@ struct WrapperSynthesizer
 
 struct WrapperKeyboard
 {
-    WrapperKeyboard(Synthesizer::Keyboard* keysptr) : pointerToKeyboard(keysptr) {}
-    ~WrapperKeyboard()
-    {
-        delete pointerToKeyboard;
-    }
+    WrapperKeyboard(Synthesizer::Keyboard* keysptr);
+    ~WrapperKeyboard();
 
     Synthesizer::Keyboard* pointerToKeyboard = nullptr;
 };
 
 struct WrapperRestaurant
 {
-    WrapperRestaurant(Restaurant* restptr) : pointerToRestaurant(restptr) {}
-    ~WrapperRestaurant()
-    {
-        delete pointerToRestaurant;
-    }
+    WrapperRestaurant(Restaurant* restptr);
+    ~WrapperRestaurant();
 
     Restaurant* pointerToRestaurant = nullptr;
 };
 
 struct WrapperKitchen
 {
-    WrapperKitchen(Restaurant::Kitchen* kitptr) : pointerToKitchen(kitptr) {}
-    ~WrapperKitchen()
-    {
-        delete pointerToKitchen;
-    }
+    WrapperKitchen(Restaurant::Kitchen* kitptr);
+    ~WrapperKitchen();
 
     Restaurant::Kitchen* pointerToKitchen = nullptr;
 };
 
 struct WrapperBank
 {
-    WrapperBank(Bank* bankptr) : pointerToBank(bankptr) {}
-    ~WrapperBank()
-    {
-        delete pointerToBank;
-    }
+    WrapperBank(Bank* bankptr);
+    ~WrapperBank();
 
     Bank* pointerToBank = nullptr;
 };
 
 struct WrapperEffectsRack
 {
-    WrapperEffectsRack(EffectsRack* fxptr) : pointerToEffectsRack(fxptr) {}
-    ~WrapperEffectsRack()
-    {
-        delete pointerToEffectsRack;
-    }
+    WrapperEffectsRack(EffectsRack* fxptr);
+    ~WrapperEffectsRack();
 
     EffectsRack* pointerToEffectsRack = nullptr;
 };
 
 struct WrapperDiningRoom
 {
-    WrapperDiningRoom(DiningRoom* drptr) : pointerToDiningRoom(drptr) {}
-    ~WrapperDiningRoom()
-    {
-        delete pointerToDiningRoom;
-    }
+    WrapperDiningRoom(DiningRoom* drptr);
+    ~WrapperDiningRoom();
 
     DiningRoom* pointerToDiningRoom = nullptr;
 };
